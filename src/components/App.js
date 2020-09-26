27fb4dd3c748d469a36b2c4e33f63faa83343fc6
jithinlal <jithtitan007@gmail.com>
2020-09-26 12:42:23 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
-import * as React from 'react'
+import * as React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { handleInitialData } from '../actions/shared';
+import Leaderboard from './Leaderboard';
+import Dashboard from './Dashboard';
+import Poll from './Poll';
+import AddPoll from './AddPoll';
+import Nav from './Nav';
 
 export default function App() {
-  return (
-    <div>
-      Redux Polls
-    </div>
-  )
+	const dispatch = useDispatch();
+	const loading = useSelector((state) => state.authedUser === null);
+
+	React.useEffect(() => {
+		dispatch(handleInitialData());
+	}, [dispatch]);
+
+	return (
+		<Router>
+			<div className='container'>
+				<Nav />
+				{loading === true ? null : (
+					<div>
+						<Route path='/' exact>
+							<Dashboard />
+						</Route>
+						<Route path='/leaderboard'>
+							<Leaderboard />
+						</Route>
+						<Route path='/polls/:id'>
+							<Poll />
+						</Route>
+						<Route path='/add'>
+							<AddPoll />
+						</Route>
+					</div>
+				)}
+			</div>
+		</Router>
+	);
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './components/App'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Loadingbar from 'react-redux-loading';
+
+import App from './components/App';
+import reducer from './reducers';
+import middleware from './middlewares';
+import './index.css';
 
 function ColorfulBorder() {
-  return (
-    <React.Fragment>
-      <ul className='border-container'>
-        <li className='border-item' style={{ background: 'var(--red)' }} />
-        <li className='border-item' style={{ background: 'var(--blue)' }} />
-        <li className='border-item' style={{ background: 'var(--pink)' }} />
-        <li className='border-item' style={{ background: 'var(--yellow)' }} />
-        <li className='border-item' style={{ background: 'var(--aqua)' }} />
-      </ul>
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<ul className='border-container'>
+				<li className='border-item' style={{ background: 'var(--red)' }} />
+				<li className='border-item' style={{ background: 'var(--blue)' }} />
+				<li className='border-item' style={{ background: 'var(--pink)' }} />
+				<li className='border-item' style={{ background: 'var(--yellow)' }} />
+				<li className='border-item' style={{ background: 'var(--aqua)' }} />
+			</ul>
+			<Loadingbar />
+		</React.Fragment>
+	);
 }
 
+const store = createStore(reducer, middleware);
+
 ReactDOM.render(
-  <React.Fragment>
-    <ColorfulBorder />
-    <App />
-  </React.Fragment>,
-  document.getElementById('root')
-)
+	<Provider store={store}>
+		<ColorfulBorder />
+		<App />
+	</Provider>,
+	document.getElementById('root'),
+);
